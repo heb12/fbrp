d0f3f1840e18221e7a453f7be4dcb8248da22978
Daniel <brikbusters@gmail.com>
2021-05-30 17:01:45 -0400

    Another test

--- test.c
+++ test.c
@@ -9,11 +9,11 @@ void debugPrint(struct Reference *ref) {
 	printf("Book: %s\n", ref->book);
 
 	for (int c = 0; c < ref->chapterLength; c++) {
-		printf("Chapter: %d-%d\n", ref->chapter[c].range[0], ref->chapter[c].range[1]);
+		printf("Chapter: [%d]-[%d]\n", ref->chapter[c].range[0], ref->chapter[c].range[1]);
 	}
 
 	for (int c = 0; c < ref->verseLength; c++) {
-		printf("Verse: %d-%d\n", ref->verse[c].range[0], ref->verse[c].range[1]);
+		printf("Verse: [%d]-[%d]\n", ref->verse[c].range[0], ref->verse[c].range[1]);
 	}
 }
 
@@ -31,5 +31,6 @@ int main() {
 	parse("2983yr92y8392r839");
 	parse("8AUW8DQ98DU98SD9W87DUASDJA");
 	parse("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!");
+	parse("John 3 4294967312"); // == John 3 16 (uint overflow)
 	return 0;
 }
